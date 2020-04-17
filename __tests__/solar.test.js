44a691402af25fed0e4f42d0c0159c925426ad46
shonburgundy <seanbeloussow@yahoo.com>
2020-04-17 14:18:51 -0700

    Mercury lifegen test PASS

--- __tests__/solar.test.js
+++ __tests__/solar.test.js
@@ -1,14 +1,17 @@
-import { Mercury } from './../src/solar-year';
+import { Planet, Mercury, Venus } from './../src/solar-year';
+
 
 describe('Mercury and its prototypes', () => {
   
   test ('should check if user age is divided by .24 & round to nearest integer', () => {
-    let mercury = new Mercury;
-    expect(mercury.ageGenerator(31)).toEqual(129)
+    expect(new Mercury().ageGenerator(31)).toEqual(129)
   });
 
    test ('should return remaining years of users life expectancy according to age', () => {
-    let mercury = new Mercury;
-    expect(mercury.lifeGenerator(25)).toEqual(55)  
+    expect(new Mercury().lifeGenerator(30)).toEqual(45)  
+  });
+
+  test ('should return remaining years of users life expectancy according to age', () => {
+    expect(new Venus().lifeGenerator(30)).toEqual(55)  
   });
 });
--- src/solar-year.js
+++ src/solar-year.js
@@ -1,16 +1,34 @@
-export class Mercury {
-  constructor(userAge, lifeExpect) {
-    this.userAge = userAge;
+export class Planet {
+  constructor(lifeExpect) {
     this.lifeExpect = lifeExpect;
+    this.yearRatio = 1;
   }
 
   ageGenerator(userAge) {
-    return Math.round(userAge/ .24)
+    return Math.round(userAge/ this.yearRatio);
   }
 
-  lifeGenerator() {
-    //if (userAge < 30) {
-    //return userAge % 80
-    //}
+  lifeGenerator(userAge) {
+    if (userAge < 30) {
+    return 80 - userAge;
+    } else if (userAge < 60) {
+      return 75 - userAge;
+    } else {
+      return 70 - userAge;
+    }
   }
 };
+
+export class Mercury extends Planet {
+  constructor(lifeExpect) {
+    super(lifeExpect)
+    this.yearRatio = .24;
+  }
+}
+
+export class Venus extends Planet {
+  constructor(lifeExpect) {
+    super(lifeExpect)
+    this.yearRatio = .62;
+  }
+}
