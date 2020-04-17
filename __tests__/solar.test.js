0147ae790e52ecaf32d1c145e0247cc8dc5653db
shonburgundy <seanbeloussow@yahoo.com>
2020-04-17 15:09:51 -0700

    ToEarthYears Tests should Fail for planet, mercury, venus

--- __tests__/solar.test.js
+++ __tests__/solar.test.js
@@ -1,17 +1,50 @@
 import { Planet, Mercury, Venus } from './../src/solar-year';
 
+//Planet
+describe('Planet and its prototypes', () => {
 
+  test ('should check if user age is divided by 1 & round to nearest integer', () => {
+    expect(new Planet().fromEarthYears(31)).toEqual(31)
+  });
+
+   test ('should return remaining years of users life expectancy according to age', () => {
+    expect(new Planet().lifeGenerator(30)).toEqual(50)  
+  });
+
+  test ('should check if user age is multiplied by 1 & round to nearest integer', () => {
+    expect(new Planet().toEarthYears(31)).toEqual(129)
+  });
+});
+
+//Mercury
 describe('Mercury and its prototypes', () => {
   
   test ('should check if user age is divided by .24 & round to nearest integer', () => {
-    expect(new Mercury().ageGenerator(31)).toEqual(129)
+    expect(new Mercury().fromEarthYears(31)).toEqual(129)
   });
 
    test ('should return remaining years of users life expectancy according to age', () => {
     expect(new Mercury().lifeGenerator(30)).toEqual(45)  
   });
 
-  test ('should return remaining years of users life expectancy according to age', () => {
-    expect(new Venus().lifeGenerator(30)).toEqual(55)  
+  test ('should check if user age is multiplied by .24 & round to nearest integer', () => {
+    expect(new Mercury().toEarthYears(129)).toEqual(129)
   });
 });
+
+//Venus
+  describe('Venus and its prototypes', () => {
+
+  test ('should return remaining years of users life expectancy according to age', () => {
+    expect(new Venus().lifeGenerator(60)).toEqual(55)  
+  });
+
+  test ('should check if user age is divided by .62 & round to nearest integer', () => {
+    expect(new Venus().fromEarthYears(31)).toEqual(129)
+  });
+
+  test ('should check if user age is multiplied by .62 & round to nearest integer', () => {
+    expect(new Venus().toEarthYears(31)).toEqual(129)
+  });
+});
+
